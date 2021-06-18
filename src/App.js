8401948acd2ce062e15512e9f45e1ea67ec36e83
Brad Davis <brad@braddavistech.com>
2021-06-18 10:43:16 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import MainIcon from "./mainIcon.png";
+import styled from "styled-components";
+import "./App.css";
+import "./main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Row>
+        <Column>
+          <Cursive>Coming Soon!</Cursive>
+          <BlockText fontSize="30px">Site Under Construction</BlockText>
+        </Column>
+        <Column>
+          <img src={MainIcon} className="App-logo" alt="logo" />
+        </Column>
+        <Column>
+          <Cursive>UX</Cursive>
+          <BlockText fontSize="65px">DESIGNER</BlockText>
+        </Column>
+      </Row>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  text-align: center;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  align-items: center;
+`;
+
+const Cursive = styled.p`
+  font-family: "Caveat", cursive;
+  font-size: 60px;
+  font-weight: 900;
+  margin: 0;
+  padding: 0;
+`;
+
+const BlockText = styled.p`
+  font-family: "Oswald", sans-serif;
+  font-weight: 900;
+  font-size: ${({ fontSize }) => fontSize && fontSize};
+  margin: 0;
+  padding: 0;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 33%;
+  margin: 0;
+`;
 
 export default App;
